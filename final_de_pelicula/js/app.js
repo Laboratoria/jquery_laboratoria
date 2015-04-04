5c66349c86b9654ddda0fbbb2861d6dbc7e7621e
andreadelrio <delrio.andre@gmail.com>
2015-04-04 16:46:36 -0500

    first commit

---
+++ final_de_pelicula/js/app.js
@@ -0,0 +1,9 @@
+$('.advertencia').click(function () {
+	$('.spoiler').toggle();
+	$(this).toggle();
+});
+
+$('.spoiler').click(function () {
+	$('.advertencia').toggle();
+	$(this).toggle();
+});
---
+++ jquery_base/js/app.js
@@ -0,0 +1,2 @@
+//Prevent spoilerphobes from seeing spoilers
+//Solution: Hide spoilers and reveal them through user ineraction
