d6d5efd9cdaaf18261c89b545f9db025516e1998
andreadelrio <delrio.andre@gmail.com>
2015-04-05 21:28:42 -0500

    galeria solution

--- galeria/js/app.js
+++ galeria/js/app.js
@@ -1,18 +1,9 @@
 //Problem: User when clicking on image goes to a dead end
 //Solution: Create an overlay with the large image - Lightbox
 
-var $overlay = $('<div id="overlay"></div>');
-var $image = $("<img>");
-var $caption = $("<p></p>");
-
-//An image to overlay
-$overlay.append($image);
-
-//A caption to overlay
-$overlay.append($caption);
-
-//Add overlay
-$("body").append($overlay);
+var $overlay = $('#overlay');
+var $image = $("#overlay img");
+var $caption = $("#overlay p");
 
 //Capture the click event on a link to an image
 $("#imageGallery a").click(function(event){
